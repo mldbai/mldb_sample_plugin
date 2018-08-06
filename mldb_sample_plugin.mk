705fa9a8a25a58d0be0b524b2b8460129544cfc6
Jeremy Barnes <jeremy@barneso.com>
2018-08-06 09:02:27 -0400

    Build order bugfix

--- mldb_sample_plugin.mk
+++ mldb_sample_plugin.mk
@@ -2,3 +2,4 @@ $(eval $(call library,mldb_sample_plugin,plugin.cc,mldb))
 $(LIB)/libmldb_sample_plugin.so: $(BIN)/mldb_runner
 
 $(eval $(call mldb_unit_test,load_plugin_demo.py))
+$(TESTS)/load_plugin_demo.py.passed:	$(LIB)/libmldb_sample_plugin.so
